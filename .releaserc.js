a728b886dd591d9a01af70bdc828076af7993d7f
krema <andre.kremser@me.com>
2022-04-30 21:13:29 +0200

    feat(release): add semantic-release

---
+++ .releaserc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    ['@semantic-release/npm',
+    {
+      npmPublish: true,
+      pkgRoot: 'out',
+    }],
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'lib/builder/package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
