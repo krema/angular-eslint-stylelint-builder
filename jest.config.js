4f932bbf816f1748eb75c28aa02c63f7c78a9f44
André Kremser <krema@users.noreply.github.com>
2022-04-30 12:53:19 +0200

    :green_heart: ci: add initial setup

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  roots: ['lib'],
   testEnvironment: 'node',
-  testPathIgnorePatterns: ["test-project", "out"]
+  testPathIgnorePatterns: ['test-project', 'out'],
+  testLocationInResults: true,
+  reporters: ['default', 'jest-junit', 'jest-github-actions-reporter'],
 };
